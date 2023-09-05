11c8eecf2b5c38a0ab0d6a42326aaf6b68cfa434
ThomasLam2142 <thomaslam2142@gmail.com>
2023-09-05 16:12:15 -0400

    basic game evaluation implemented.

--- memorygame.js
+++ memorygame.js
@@ -1,11 +1,42 @@
 let firstCard = 0;
 let secondCard = 0;
 
+let gameState = 0;
+
 function buttonPress(cardNum){
-    firstCard = cardNum;
-    console.log(firstCard)
+    
+    if (gameState == 0){
+        firstCard = cardNum;
+        gameState = 1;
+        console.log(firstCard);
+    }
+    else if(gameState == 1){
+        secondCard = cardNum;
+        gameState = 2;
+        console.log(secondCard);
+    }
+
+
+
 }
 
 function checkBtnClick(){
-    console.log("check button clicked")
+    if (gameState == 2){
+        console.log("Evaluating result")
+
+        if (firstCard == secondCard){
+            alert("Correct!");
+        }
+
+        if (firstCard != secondCard){
+            alert("Wrong!");
+        }
+    }else if (gameState==1){
+        alert("Select one more option")
+    }
+    else if (gameState==0){
+        alert("Select 2 options")
+    }
+    
 }
+
