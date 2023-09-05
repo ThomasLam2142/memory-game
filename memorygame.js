18b0641fdae72044a4be301362f1e6eba36f09ef
ThomasLam2142 <thomaslam2142@gmail.com>
2023-09-05 16:14:21 -0400

    started game scoring system

--- memorygame.js
+++ memorygame.js
@@ -2,6 +2,7 @@ let firstCard = 0;
 let secondCard = 0;
 
 let gameState = 0;
+let gameScore = 0;
 
 function buttonPress(cardNum){
     
@@ -22,20 +23,24 @@ function buttonPress(cardNum){
 
 function checkBtnClick(){
     if (gameState == 2){
-        console.log("Evaluating result")
+        console.log("Evaluating result");
 
         if (firstCard == secondCard){
             alert("Correct!");
+            gameState = 0;
+            gameScore += 1;
         }
 
         if (firstCard != secondCard){
             alert("Wrong!");
+            gameState = 0;
+            gameScore -= 1;
         }
     }else if (gameState==1){
-        alert("Select one more option")
+        alert("Select one more option");
     }
     else if (gameState==0){
-        alert("Select 2 options")
+        alert("Select 2 options");
     }
     
 }
